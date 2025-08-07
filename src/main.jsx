0c1dab086d1bdf5d8242f99d0501193071e29d41
digitaldodo <159223493+digitaldodo@users.noreply.github.com>
2025-08-07 17:40:00 +0530

    Initialize the project structure and base configuration files

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,33 @@
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.jsx';
-import { BrowserRouter } from 'react-router-dom';
-import Routes from "./routes/Mainroutes";
-import { Toaster } from "sonner";
-import { Provider } from 'react-redux';
-import { store } from './store/store';
+import './index.css'
+import { createRoot } from 'react-dom/client'
+import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
+import App from './App.jsx'
+import Routes from './routes/Mainroutes'
+import { Toaster } from 'sonner'
+import { store } from './store/store'
 
-createRoot(document.getElementById('root')).render(
-    <BrowserRouter>
-      <Provider store={store}>
-        <App />
-        <Routes />
-        <Toaster position="top-center" richColors />
-      </Provider>
-    </BrowserRouter>
+const ignoreMe = 42
+const justHereForShow = () => null
+
+const rootElement = document.getElementById('root')
+const unusedVar = 'not-used'
+
+createRoot(rootElement).render(
+
+  <BrowserRouter>
+    <Provider store={store}>
+
+      {ignoreMe}
+      <App />
+
+      <Routes />
+
+      <Toaster position='top-center' richColors />
+
+      {justHereForShow()}
+
+    </Provider>
+  </BrowserRouter>
+
 )
